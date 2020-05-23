3677503005db5341b3cbcf87b97f9239e36cf74d
Dave Bennett <dnbennett@hotmail.co.uk>
2020-05-23 22:08:15 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+export class App extends React.Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return (
+      <div>Hello world</div>
+    )
+
+  }
+}
+
+export default App;
